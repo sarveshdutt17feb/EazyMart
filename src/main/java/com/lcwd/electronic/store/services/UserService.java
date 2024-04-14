855ac862af1e1e5b76a7754aa06b605326497e40
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-14 15:24:53 +0530

    creating UserRepository and for db operation entity calss object is used and for data transfer from controller dtos is used

---
+++ src/main/java/com/lcwd/electronic/store/services/UserService.java
@@ -0,0 +1,19 @@
+package com.lcwd.electronic.store.services;
+
+import com.lcwd.electronic.store.entities.User;
+
+public interface UserService {
+    //create
+     User createUser(User user);
+    //update
+
+    //delete
+
+    // get all user
+
+    //get single user
+
+    //search user
+
+    //other user specific features provide here
+}
