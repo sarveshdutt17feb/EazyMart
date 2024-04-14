3a10e74b0a966e3004d8db60c6808d247409e2da
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-14 16:30:20 +0530

    user service | understanding dtos

--- src/main/java/com/lcwd/electronic/store/services/UserService.java
+++ src/main/java/com/lcwd/electronic/store/services/UserService.java
@@ -1,19 +1,27 @@
 package com.lcwd.electronic.store.services;
 
+import com.lcwd.electronic.store.dtos.UserDto;
 import com.lcwd.electronic.store.entities.User;
+
+import java.util.List;
 
 public interface UserService {
     //create
-     User createUser(User user);
+     UserDto createUser(UserDto userDto);
     //update
-
+    UserDto updateUser(UserDto userDto,String userId);
     //delete
+    void deleteUser(String userId);
 
     // get all user
+    List<UserDto> getAllUser();
 
     //get single user
-
+    UserDto getSingleUser(String userId);
+    //get single user by email
+    UserDto getSingleUserByEmail(String email);
     //search user
+    List<UserDto> searchUser(String keyword);
 
     //other user specific features provide here
 }
