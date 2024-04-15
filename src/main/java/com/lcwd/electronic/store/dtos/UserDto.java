5a815fd4f9d649a1042895ff65ff78a76a22d9ce
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-15 16:10:22 +0530

    @Pattern for regular expression for validation

--- src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
+++ src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -17,10 +18,10 @@ public class UserDto {
     private String userId;
     @Size(min=3,max=20,message = "invalid name !!")
     private String name;
-
-    @Email(message = "invalid user email !!")
-    @NotBlank(message = "email is required !!")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$", message = "Invalid User Email !!")
+    @NotBlank(message = "Email is required !!")
     private String email;
+
     @NotBlank(message = "password is required !!")
     private String password;
     @Size(min=4,max=6,message = "invalid gender !!")
