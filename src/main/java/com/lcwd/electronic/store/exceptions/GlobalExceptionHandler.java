e261b1679d377d19f3b399509086e0fe9d5f53db
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-15 19:20:49 +0530

    handling exception of validation api MethodArgumentNotValidException in GlobalExceptionHandler

--- src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +26,19 @@ public class GlobalExceptionHandler {
         logger.info("exception handler invoked!!");
         ApiResponseMessage response =  ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.NOT_FOUND).success(true).build();
        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    }
+    //for MethodArgumentNotValidException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        //it will contsin list of errors here we can get error one by one
+
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        Map<String,Object> response = new HashMap<>();
+         allErrors.stream().forEach((objectError -> {
+             String message =objectError.getDefaultMessage();
+             String field = ((FieldError) objectError).getField();
+             response.put(field,message);
+         }));
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 }
