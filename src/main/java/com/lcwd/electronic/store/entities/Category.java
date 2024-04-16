cd67024ad02d8c1ec94995e1baf5f0cb8626a284
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-16 11:29:38 +0530

    Category Module |Creating Entity and Dto with validation

---
+++ src/main/java/com/lcwd/electronic/store/entities/Category.java
@@ -0,0 +1,30 @@
+package com.lcwd.electronic.store.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="categories")
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    private String categoryId;
+    @Column(name="category_title",length = 60,nullable = false)
+    private String title;
+    @Column(name="category_desc",length = 50)
+    private String description;
+    private String coverImage;
+    //other property if has
+
+}
