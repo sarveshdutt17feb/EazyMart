8110089dcd7e4957ace929c5982b748d133fb403
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-17 18:54:32 +0530

    creating ProductController | creating product apis

---
+++ src/main/java/com/lcwd/electronic/store/controllers/ProductController.java
@@ -0,0 +1,93 @@
+package com.lcwd.electronic.store.controllers;
+
+import com.lcwd.electronic.store.dtos.ApiResponseMessage;
+import com.lcwd.electronic.store.dtos.PageableResponse;
+import com.lcwd.electronic.store.dtos.ProductDto;
+import com.lcwd.electronic.store.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    //create
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+        ProductDto productDto1 = productService.create(productDto);
+        return new ResponseEntity<>(productDto1, HttpStatus.CREATED);
+    }
+
+    //update
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productDto, @PathVariable("productId") String productId) {
+        ProductDto updatedProduct = productService.update(productDto, productId);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+
+    //delete
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<ApiResponseMessage> deleteProduct(@PathVariable String productId){
+        productService.delete(productId);
+        ApiResponseMessage response = ApiResponseMessage.builder().message("Product is deleted successfully !!").status(HttpStatus.OK).success(true).build();
+        return ResponseEntity.ok(response);
+    }
+
+    //get single
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable String productId) {
+        ProductDto productDto = productService.get(productId);
+
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+    //getAllLive
+    @GetMapping
+    public ResponseEntity<PageableResponse<ProductDto>> getAllLive(
+            @RequestParam(value = "pageNumber",defaultValue = "0",required = false)  int pageNumber,
+            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "title",required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir
+
+    ){
+        PageableResponse<ProductDto> allLive = productService.getAllLive(pageNumber, pageSize, sortBy, sortDir);
+
+        return new ResponseEntity<>(allLive,HttpStatus.OK);
+    }
+
+    //getAll
+    @GetMapping
+    public ResponseEntity<PageableResponse<ProductDto>> getAll(
+            @RequestParam(value = "pageNumber",defaultValue = "0",required = false)  int pageNumber,
+            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "title",required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir
+
+    ){
+        PageableResponse<ProductDto> allProducts = productService.getAll(pageNumber, pageSize, sortBy, sortDir);
+
+        return new ResponseEntity<>(allProducts,HttpStatus.OK);
+    }
+    //search all
+
+    @GetMapping("/search/{query}")
+    public ResponseEntity<PageableResponse<ProductDto>> searchProduct(
+            @PathVariable String query,
+            @RequestParam(value = "pageNumber",defaultValue = "0",required = false)  int pageNumber,
+            @RequestParam(value = "pageSize",defaultValue = "10",required = false) int pageSize,
+            @RequestParam(value = "sortBy",defaultValue = "title",required = false) String sortBy,
+            @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir
+
+    ){
+        PageableResponse<ProductDto> allProducts = productService.searchByTitle(query,pageNumber, pageSize, sortBy, sortDir);
+
+        return new ResponseEntity<>(allProducts,HttpStatus.OK);
+    }
+
+
+
+}
